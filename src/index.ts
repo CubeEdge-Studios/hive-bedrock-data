06c4430ba8d638777dd8d0538fdd738b3d664564
UCDFiddes <66165184+UCDFiddes@users.noreply.github.com>
2023-11-14 22:05:43 +0000

    Fix Types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { GAMES } from "./games";
-
 import calculateLevelFromXP from "./math/calculateLevelFromXP";
 import calculateXPFromLevel from "./math/calculateXPFromLevel";
 import calculateLevelPercentageComplete from "./math/calculateLevelPercentageComplete";
 import calculatePercentageComplete from "./math/calculatePercentageComplete";
 import calculateLevelXP from "./math/calculateLevelXP";
 import hasCompleted from "./math/hasCompleted";
+import { GAME, GAME_MODE_QUEUE_TYPE, GAME_MODE_TYPE } from "./types/GAMES";
 
 export {
     GAMES,
@@ -15,4 +15,7 @@ export {
     calculateLevelXP,
     calculateLevelPercentageComplete,
     hasCompleted,
+    GAME,
+    GAME_MODE_QUEUE_TYPE,
+    GAME_MODE_TYPE,
 };
