1ba3704f929f59b592bf701baffcaafef52c69c8
UCDFiddes <66165184+UCDFiddes@users.noreply.github.com>
2023-11-16 19:44:00 +0000

    Fix CDN url

--- src/games/index.ts
+++ src/games/index.ts
@@ -1,5 +1,7 @@
 import { GAME } from "../types/GAMES";
 
+export const IMAGE_CDN = "https://cdn.playhive.com";
+
 import bridge from "./bridge";
 import build from "./build";
 import ctf from "./ctf";
@@ -13,8 +15,6 @@ import party from "./party";
 import sg from "./sg";
 import sky from "./sky";
 import wars from "./wars";
-
-export const IMAGE_CDN = "https://cdn.playhive.com";
 
 export const GAMES = {
     [GAME.BlockDrop]: drop,
